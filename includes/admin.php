3c789e60afac4164d3d2d9be6ffcd9c6d8ec2516
Gary Jones <gary@garyjones.co.uk>
2014-08-04 12:18:17 +0100

    Improve user interface text
    * Marks up the help text as description, and moves it to below the settings.
    * Simplifies the labels.
    * Gives less emphasis to Mailchimp, since the same plugin could be used to make the same amendments and let another service do the actual sending of emails from an RSS feed.

--- includes/admin.php
+++ includes/admin.php
@@ -20,20 +20,20 @@ class Add_MailChimp_Settings {
 		register_setting( 'media', 'mailchimp_alternate_feed', 'esc_attr' );
 		add_settings_section(
 			'send_rss_section',
-			'MailChimp Media Settings',
+			'RSS Feeds',
 			array( &$this, 'send_rss_section_callback'),
 			'media'
 		);
 		add_settings_field(
 			'mailchimp_image_size_setting',
-			'<label for="mailchimp_image_size">' . __( 'MailChimp/RSS feed image size:' , 'send_images_rss' ) . '</label>',
+			'<label for="mailchimp_image_size">' . __( 'RSS image size:' , 'send_images_rss' ) . '</label>',
 			array( &$this, 'send_rss_field' ),
 			'media',
 			'send_rss_section'
 		);
 		add_settings_field(
 			'mailchimp_alternate_rss_feed',
-			'<label for="mailchimp_alternate_feed">' . __( 'Alternate Feed Method?' , 'send_images_rss' ) . '</label>',
+			'<label for="mailchimp_alternate_feed">' . __( 'Alternate feed?' , 'send_images_rss' ) . '</label>',
 			array( &$this, 'send_rss_alternate' ),
 			'media',
 			'send_rss_section'
@@ -41,7 +41,7 @@ class Add_MailChimp_Settings {
 	}
 
 	public function send_rss_section_callback() {
-		echo '<p>The Send Images to RSS plugin works out of the box without changing any settings. However, if you want to customize your image size and do not want to change the default feed, change those items here.</p>';
+		echo '<p>The <i>Send Images to RSS</i> plugin works out of the box without changing any settings. However, if you want to customize your image size and do not want to change the default feed, change those items here.</p>';
 	}
 	/**
 	 * HTML for extra settings
@@ -52,20 +52,21 @@ class Add_MailChimp_Settings {
 			$value = 560;
 		}
 
-		echo '<p> Most users <strong>should not</strong> need to change this number, but if you have customized your emails to be a different width, or you are using a template with a sidebar, you will want to change the width here. The default width is 560 pixels, which is the content width of a standard single column email (600 pixels wide with 20 pixels padding on the content).</p><br />';
+		echo '<label for="mailchimp_image_size">Max Width</label>';
 		echo '<input type="number" step="1" min="200" id="mailchimp_image_size" name="mailchimp_image_size" value="' . esc_attr( $value ) . '" class="small-text" />';
-		echo '<label for="mailchimp_image_size"> <strong>Note:</strong> Changing the width here will not affect previously uploaded images, but it will affect the max-width applied to images&rsquo; style.</label>';
+		echo '<p class="description">Most users should <strong>should not</strong> need to change this number, but if you have customized your emails to be a different width, or you are using a template with a sidebar, you will want to change the width here. The default width is 560 pixels, which is the content width of a standard single column email (600 pixels wide with 20 pixels padding on the content).</p>';
+		echo '<p class="description"><strong>Note:</strong> Changing the width here will not affect previously uploaded images, but it will affect the max-width applied to images&rsquo; style.</p>';
 
 	}
 
 	public function send_rss_alternate() {
 		$value = get_option( 'mailchimp_alternate_feed' );
 
-		echo '<p>By default, the Send Images to RSS plugin modifies every feed from your site. If you want to leave your main feed untouched and set up a totally separate feed for emails/MailChimp only, check this box.</p><br />';
-		echo '<input type="checkbox" name="mailchimp_alternate_feed" id="mailchimp_alternate_feed" value="1"' . checked( 1, $value, false ) . ' class="code" /> <label for="mailchimp_alternate_feed">If checked, this will <em>disable</em> the plugin for all feeds except for a custom feed, which will be for the main blog feed.</label>';
+		echo '<input type="checkbox" name="mailchimp_alternate_feed" id="mailchimp_alternate_feed" value="1"' . checked( 1, $value, false ) . ' class="code" /> <label for="mailchimp_alternate_feed">Apply sizes only to new custom feed</label>';
+		echo '<p class="description">By default, the Send Images to RSS plugin modifies every feed from your site. If you want to leave your main feed untouched and set up a totally separate feed for emails/MailChimp only, check this box.</p><br />';
 
 		if ( $value === '1' ) {
-			echo '<br /><p>Hey! Your new feed is at <a href="' . home_url() . '/feed/?custom=email" target="_blank">' . home_url() . '/feed/?custom=email</a>.</p>';
+			echo '<p>Hey! Your new feed is at <a href="' . home_url() . '/feed/?custom=email" target="_blank">' . home_url() . '/feed/?custom=email</a>.</p>';
 		}
 		elseif ( $value === '' ) {
 			echo '<p>The checkbox is NOT checked.</p>'; // this line could be tossed but have it in my working code for my own referece.
