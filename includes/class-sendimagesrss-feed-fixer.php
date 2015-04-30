6032714d1a45df1c1c53f195b7d15b5423e8eaa3
Robin Cornett <hello@robincornett.com>
2015-04-30 09:21:39 -0400

    Refactor get_image_id
    Sets a fallback if original image has number pattern like WP's resized
    images in the URL. Mad props to @hellofromtonya to abstract the SQL
    query and make it more efficient.

--- includes/class-sendimagesrss-feed-fixer.php
+++ includes/class-sendimagesrss-feed-fixer.php
@@ -274,7 +274,7 @@ class SendImagesRSS_Feed_Fixer {
 	 * @link   http://philipnewcomer.net/2012/11/get-the-attachment-id-from-an-image-url-in-wordpress/
 	 */
 	protected function get_image_id( $attachment_url = '' ) {
-		global $wpdb;
+
 		$attachment_id = false;
 
 		// If there is no url, return.
@@ -288,17 +288,46 @@ class SendImagesRSS_Feed_Fixer {
 		// Make sure the upload path base directory exists in the attachment URL, to verify that we're working with a media library image
 		if ( false !== strpos( $attachment_url, $upload_dir_paths['baseurl'] ) ) {
 
-			// If this is the URL of an auto-generated thumbnail, get the URL of the original image
-			$attachment_url = preg_replace( '/-\d+x\d+(?=\.(jpg|jpeg|png|gif)$)/i', '', $attachment_url );
-
 			// Remove the upload path base directory from the attachment URL
 			$attachment_url = str_replace( $upload_dir_paths['baseurl'] . '/', '', $attachment_url );
 
+			// If this is the URL of an auto-generated thumbnail, get the URL of the original image
+			$url_stripped   = preg_replace( '/-\d+x\d+(?=\.(jpg|jpeg|png|gif)$)/i', '', $attachment_url );
+
 			// Finally, run a custom database query to get the attachment ID from the modified attachment URL
-			$attachment_id = $wpdb->get_var( $wpdb->prepare( "SELECT wposts.ID FROM $wpdb->posts wposts, $wpdb->postmeta wpostmeta WHERE wposts.ID = wpostmeta.post_id AND wpostmeta.meta_key = '_wp_attached_file' AND wpostmeta.meta_value = '%s' AND wposts.post_type = 'attachment'", $attachment_url ) );
+			$attachment_id  = $this->fetch_image_id_query( $url_stripped, $attachment_url );
 
 		}
 
 		return $attachment_id;
 	}
+
+	/**
+	 * Fetch image ID from database
+	 * @param  var $url_stripped   image url without WP resize string (eg 150x150)
+	 * @param  var $attachment_url image url
+	 * @return int (image id)                 image ID, or false
+	 *
+	 * @since 2.2.0
+	 *
+	 * @author hellofromtonya
+	 */
+	protected function fetch_image_id_query( $url_stripped, $attachment_url ) {
+
+		global $wpdb;
+
+		$query_sql = $wpdb->prepare(
+			"
+				SELECT wposts.ID
+				FROM {$wpdb->posts} wposts, {$wpdb->postmeta} wpostmeta
+				WHERE wposts.ID = wpostmeta.post_id AND wpostmeta.meta_key = '_wp_attached_file' AND wpostmeta.meta_value IN ( %s, %s ) AND wposts.post_type = 'attachment'
+			",
+			$url_stripped, $attachment_url
+		);
+
+		$result = $wpdb->get_col( $query_sql );
+
+		return empty( $result ) || ! is_numeric( $result[0] ) ? false : intval( $result[0] );
+	}
+
 }
