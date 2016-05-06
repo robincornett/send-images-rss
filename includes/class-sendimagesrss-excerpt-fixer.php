1e69e4211da5f46abe60886d014f6832d9f067de
Robin Cornett <hello@robincornett.com>
2016-05-06 10:31:31 -0400

    Fix early return on excerpt

--- includes/class-sendimagesrss-excerpt-fixer.php
+++ includes/class-sendimagesrss-excerpt-fixer.php
@@ -24,7 +24,7 @@ class SendImagesRSS_Excerpt_Fixer {
 	 */
 	public function do_excerpt( $content ) {
 		if ( ! is_feed() ) {
-			return;
+			return $content;
 		}
 		$this->setting  = sendimagesrss_get_setting();
 		/**
